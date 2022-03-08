d85314755b92cde31a7e30a408001d96d8b66228
Mandeep Bose <94315656+Mandeep-Bose@users.noreply.github.com>
2022-03-08 10:50:45 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,85 @@
+// switch case
+
+// first type of scenario
+// we will compare the value of the variable directly
+
+// let a = 17;
+
+// switch (a){
+//     case 10:
+//         console.log("Value is 10");
+//         break;  //breaks the flow
+//     case 15:
+//         console.log("Value is 15");
+//         break;
+//     default:
+//         console.log("default case");
+//         break;
+// }
+
+// switch (a){
+//     case a>0:
+//         console.log("Positive");
+//         break;
+//     case a<0:
+//         console.log("Negative");
+//         break;
+//     default:
+//         console.log("Zero Value");
+//         break;
+// }
+
+// let i=1;
+// for(;i<=10;i++);{
+// console.log(2*i);
+// }
+
+// let i = 1;
+// for (; i <= 10;) {
+//     console.log(2 * i)
+//     i++
+// }
+
+// let i = 0
+// while (i < 10) {
+//     console.log("value of i is", i);
+//     i++
+// }
+// cconsole.log("outside loop")
+
+// let i = 0
+// do {
+//     console.log("value of i is", i);
+//     i++;
+// }
+// while (i < 10);
+
+// for(let i=1;i<5;i++){
+//   for( let j=0;j<2;j++){
+//     console.log("the value of i is "+ i +" and value of j is "+ j);
+//   }
+// }
+
+// let a=3;
+// let b=4;
+// function add(){
+//   console.log(a+b);
+// }
+
+// function sub(a,b){
+//   console.log(a-b);
+// }
+
+// function mul(a,b){
+//   return a*b;
+// }
+
+// const div = (a,b) => {
+//   return a/b;
+// }
+
+
+// add();
+// sub(8,5);
+// console.log(mul(4,8));
+// console.log(div(9,3));
